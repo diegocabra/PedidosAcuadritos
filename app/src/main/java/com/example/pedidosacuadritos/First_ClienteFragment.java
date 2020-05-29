48b2e169d03cca85b1c00f1a030917b3544ca7ac
DgoCabra <Diego.m876@gmail.com>
2020-05-28 22:04:16 -0300

    No recuerdo que hice, pero no anda. xD

--- app/src/main/java/com/example/pedidosacuadritos/First_ClienteFragment.java
+++ app/src/main/java/com/example/pedidosacuadritos/First_ClienteFragment.java
@@ -25,7 +25,7 @@ public class First_ClienteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cliente, container, false);
+        return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
 }
--- app/src/main/java/com/example/pedidosacuadritos/Second_ProductoFragment.java
+++ app/src/main/java/com/example/pedidosacuadritos/Second_ProductoFragment.java
@@ -25,7 +25,7 @@ public class Second_ProductoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_producto, container, false);
+        return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
 }
